0fb8b3183d4454104639e04dfde591022c4c8514
Dillon Godwin <ddgodwin01@byui.edu>
2022-05-13 19:02:07 -0600

    W04 Assignment

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../documents.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document('1', 'Dillon', 'Description', 'url'),
+    new Document('2', 'Joe', 'Description', 'url'),
+    new Document('3', 'Bob', 'Description', 'url'),
+    new Document('4', 'Steve', 'Description', 'url')
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
