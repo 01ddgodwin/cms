fbe0f3fc581be53b688f51817f5810e38f9f7c7c
Dillon Godwin <ddgodwin01@byui.edu>
2022-05-13 19:12:43 -0600

    W04 Assignment

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -9,10 +9,10 @@ import { Document } from '../documents.model';
 export class DocumentListComponent implements OnInit {
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
   documents: Document[] = [
-    new Document('1', 'Dillon', 'Description', 'url'),
-    new Document('2', 'Joe', 'Description', 'url'),
-    new Document('3', 'Bob', 'Description', 'url'),
-    new Document('4', 'Steve', 'Description', 'url')
+    new Document('1', 'WDD 130 - Web Fundamentals', 'This course introduces students to the World Wide Web and to careers in web site design and development.', 'url'),
+    new Document('2', 'WDD 230 - Web Frontend Development I', 'This course focuses on the planning and development of responsive web sites using HTML, CSS, and JavaScript with attention to usability principles.', 'url'),
+    new Document('3', 'WDD 330 - Web Frontend Development II', 'WDD 330 will continue with the topics presented in WDD 230 Web Front-end Development I.', 'url'),
+    new Document('4', 'WDD 430 - Web Full-Stack Development', 'his course will teach you how to design and build interactive web based applications using HTML, CSS, JavaScript, and a web development stack.', 'url')
   ];
 
   constructor() { }
